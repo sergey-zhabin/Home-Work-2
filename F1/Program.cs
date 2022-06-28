b5b152c7f552661ce3c74ce3273dbcb476a70c99
Sergey <santexnik.86@mail.ru>
2022-06-28 23:46:30 +0300

    Решение 1 задачи дз

---
+++ F1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+Zada4a_13(number);
+void Zada4a_13(string arg)
+{
+    if (arg.Length < 3 )
+    Console.WriteLine(" третьей цифры нет ");
+    else
+    Console.WriteLine(arg[2]);
+}
